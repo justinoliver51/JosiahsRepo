8c32bef1ebca8ccc09de5cc3ee736ad73c5fcea0
User <user@b40904-11.gateway.2wire.net>
2013-08-08 18:47:32 -0500

    Initial commit

---
+++ JosiahsProjects/JosiahsProjects/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  JosiahsProjects
+//
+//  Created by User on 8/8/13.
+//  Copyright (c) 2013 Engineering. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
