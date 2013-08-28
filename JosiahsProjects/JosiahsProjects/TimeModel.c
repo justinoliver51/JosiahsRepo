1deb84b6997ca044bf94fb178cf6d3d97e13cd41
User <user@mac-mini.gateway.2wire.net>
2013-08-27 21:38:31 -0500

    whatever

---
+++ JosiahsProjects/JosiahsProjects/TimeModel.c
@@ -0,0 +1,19 @@
+//
+//  TimeModel.c
+//  JosiahsProjects
+//
+//  Created by User on 8/27/13.
+//  Copyright (c) 2013 Engineering. All rights reserved.
+//
+
+#include <stdio.h>
+void updateTime(int minute, int second)
+{
+    for(second = 0; second <= 60; second = second + 1)
+    second = second + 1
+    if(second = 60)
+    {
+        second = 0
+        minute = minute + 1
+    }
+    {return;
