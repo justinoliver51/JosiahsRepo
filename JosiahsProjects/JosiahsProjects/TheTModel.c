1755ee09553651cbbd1b997be7a2e21ec1623029
User <user@mac-mini.gateway.2wire.net>
2013-09-05 20:04:35 -0500

    t model

---
+++ JosiahsProjects/JosiahsProjects/TheTModel.c
@@ -0,0 +1,147 @@
+//
+//  TModel.c
+//  JosiahsProjects
+//
+//  Created by User on 8/30/13.
+//  Copyright (c) 2013 Engineering. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "TheTModel.h"
+
+#define NUMBER_OF_BTOCKS 4
+
+// FUNCTION DECTARATIONS
+void rotate();
+void rotateT();
+void freeTModel(void *);
+void initializeTPoints(int x, int y);
+
+// PRIVATE VARIABTES
+// this is the array containing the locations for each of our blocks
+static Location positionArray[NUMBER_OF_BTOCKS];
+static ShapeModelPtr shapePtr;
+
+TModelPtr initializeTModel()
+{
+    // INITIATIZE GTOBATS
+    // Our initial score is 0
+    
+    // Initialize the struct that represents our ScoreBoard
+    TModelPtr lPtr = (TModelPtr) malloc(sizeof( TModel ) );  // Allocates memory for T
+    
+    shapePtr = initializeShapeModel();
+    shapePtr->rotate = &rotateT;
+    shapePtr->initializePoints = &initializeTPoints;
+    lPtr->free = &freeTModel;
+    
+    // Initialize the shape
+    // The board will initialize the offsets
+    positionArray[0].x = 1;
+    positionArray[0].y = 0;
+    positionArray[1].x = 2;
+    positionArray[1].y = 0;
+    positionArray[2].x = 2;
+    positionArray[2].y = 1;
+    positionArray[3].x = 2;
+    positionArray[3].y = 2;
+    
+    return lPtr;
+}
+
+// this functions translates the T shape to the coordinate (x,y)
+void initializeTPoints(int x, int y)
+{
+    positionArray[0].x = positionArray[0].x + x;
+    positionArray[0].y = positionArray[0].y + y;
+    positionArray[1].x = positionArray[1].x + x;
+    positionArray[1].y = positionArray[1].y + y;
+    positionArray[2].x = positionArray[2].x + x;
+    positionArray[2].y = positionArray[2].y + y;
+    positionArray[3].x = positionArray[3].x + x;
+    positionArray[3].y = positionArray[3].y + y;
+    
+    
+    return;
+}
+// There are two positions for a T, vertical or horizontal.
+// There are two positions for a T, vertical or horizontal.
+void rotateT()
+{
+    if(shapePtr->getState() == 0)
+    {
+        
+        positionArray[0].x = positionArray[0].x;
+        positionArray[0].y = positionArray[0].y;
+        positionArray[1].x = positionArray[1].x;
+        positionArray[1].y = positionArray[1].y;
+        positionArray[2].x = positionArray[2].x - 2;
+        positionArray[2].y = positionArray[2].y;
+        positionArray[3].x = positionArray[3].x - 2;
+        positionArray[3].y = positionArray[3].y - 2;
+        
+        shapePtr->setState(1);
+    }
+    
+    else if(shapePtr->getState() == 1)
+    {
+        
+        positionArray[0].x = positionArray[0].x - 1;
+        positionArray[0].y = positionArray[0].y + 2;
+        positionArray[1].x = positionArray[1].x - 1;
+        positionArray[1].y = positionArray[1].y + 2;
+        positionArray[2].x = positionArray[2].x;
+        positionArray[2].y = positionArray[2].y;
+        positionArray[3].x = positionArray[3].x;
+        positionArray[3].y = positionArray[3].y;
+        
+        shapePtr->setState(2);
+    }
+    
+    else if(shapePtr->getState() == 2)
+    {
+        
+        positionArray[0].x = positionArray[0].x + 1;
+        positionArray[0].y = positionArray[0].y - 1;
+        positionArray[1].x = positionArray[1].x + 1;
+        positionArray[1].y = positionArray[1].y - 1;
+        positionArray[2].x = positionArray[2].x;
+        positionArray[2].y = positionArray[2].y;
+        positionArray[3].x = positionArray[3].x;
+        positionArray[3].y = positionArray[3].y;
+        
+        shapePtr->setState(3);
+    }
+    
+    else
+    {
+        
+        positionArray[0].x = positionArray[0].x;
+        positionArray[0].y = positionArray[0].y - 1;
+        positionArray[1].x = positionArray[1].x;
+        positionArray[1].y = positionArray[1].y - 1;
+        positionArray[2].x = positionArray[2].x + 2;
+        positionArray[2].y = positionArray[2].y;
+        positionArray[3].x = positionArray[3].x + 2;
+        positionArray[3].y = positionArray[3].y + 2;
+        
+        shapePtr->setState(0);
+    }
+    
+    return;
+}
+
+
+// Frees the allocated memory of the Score Model
+
+
+// Frees the allocated memory of the Score Model
+
+void freeTModel(void *lModel)
+{
+    
+    TModelPtr lModelPtr = (TModelPtr) lModel;
+    shapePtr->free(shapePtr);
+    free(lModelPtr);
+}
---
+++ JosiahsProjects/JosiahsProjects/TheTModel.h
@@ -0,0 +1,88 @@
+//
+//  TModel.h
+//  JosiahsProjects
+//
+//  Created by User on 8/30/13.
+//  Copyright (c) 2013 Engineering. All rights reserved.
+//
+
+#ifndef JosiahsProjects_TModel_h
+#define JosiahsProjects_TModel_h
+
+#include "ShapeModel.h"
+
+typedef struct
+{
+    ShapeModelPtr shapePtr;
+    
+    // Free the score model
+    void (* free)(void *);
+} TModel, *TModelPtr;
+
+/*
+ State 0
+     y
+        ___ ___ ___ 
+       |   |   |   |
+     2 |   |   | 3 |
+       |___|___|___|
+       |   |   |   |
+     1 |   |   | 2 |
+       |___|___|___|
+       |   |   |   |
+     0 |   | O | 1 |
+       |___|___|___|
+ 
+         0   1   2     x
+ 
+
+ State 1
+      y
+        ___ ___ ___ 
+       |   |   |   |
+     2 |   |   |   |
+       |___|___|___|
+       |   |   |   |
+     1 | 2 |   |   |
+       |___|___|___|
+       |   |   |   |
+     0 | 3 | 0 | 1 |
+       |___|___|___|
+ 
+         0   1   2     x
+
+ 
+ State 2
+     y
+        ___ ___ ___ 
+       |   |   |   |
+     2 | O | 1 |   |
+       |___|___|___|
+       |   |   |   |
+     1 | 2 |   |   |
+       |___|___|___|
+       |   |   |   |
+     0 | 3 |   |   |
+       |___|___|___|
+ 
+         0   1   2     x
+ 
+ 
+ State 3
+     y
+        ___ ___ ___ 
+       |   |   |   |
+     2 |   |   |   |
+       |___|___|___|
+       |   |   |   |
+     1 | 2 | 0 | 1 |
+       |___|___|___|
+       |   |   |   |
+     0 | 3 |   |   |
+       |___|___|___|
+ 
+         0   1   2     x
+*/
+
+
+#endif
