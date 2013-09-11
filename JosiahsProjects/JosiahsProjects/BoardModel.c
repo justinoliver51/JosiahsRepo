c7e1aa1bb68a05e6b770fac2307d360f43b6c047
Justin Oliver <justinoliver51@yahoo.com>
2013-09-10 19:40:17 -0500

    Joe do your homework.

--- JosiahsProjects/JosiahsProjects/BoardModel.c
+++ JosiahsProjects/JosiahsProjects/BoardModel.c
@@ -7,3 +7,14 @@
 //
 
 #include <stdio.h>
+
+/*
+⁃	falling shape
+⁃	fall speed
+⁃	2-dimensional array of chars representing the board
+⁃	xlen - number of blocks long in x
+⁃	ylen - number of blocks long in y
+*/
+
+int a[5];  // Example of a one-dimensional array
+int a1[5][10]; // example of a two dimensional array representing x coordinate and y coordinate
--- JosiahsProjects/JosiahsProjects/BoardModel.h
+++ JosiahsProjects/JosiahsProjects/BoardModel.h
@@ -13,31 +13,11 @@
 
 /*
  BoardModel's responsibilities:
- ⁃	falling shape
- ⁃	fall speed
- ⁃	2-dimensional array of chars representing the board
- ⁃	xlen - number of blocks long in x
- ⁃	ylen - number of blocks long in y
  ⁃	fall( )
  ⁃	destroyLine( ) --- moves other li
  ⁃	lockShape( )
  ⁃	interrupt 2: updateBoard( )
 */
-
- //Example struct:
- // This struct represents the scoreboard
-typedef struct
-{
-    // Updates the score
-    void (* updateScore)(int, int);
-    
-    // Gets the current score
-    unsigned long (* getScore)(void);
-    
-    // Free the score model
-    void (* free)(void *);
-} ScoreModel, *ScoreModelPtr;
-
 
 
 typedef struct
